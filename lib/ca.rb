33f1e58f599bfd3590fb2ecd7acc5ffe26876918
Wojciech Bozek <wbozek@fractalsoft.org>
2012-11-06 15:39:42 +0100

    TexaAnalitics class initialize

--- lib/ca.rb
+++ lib/ca.rb
@@ -1,5 +1,32 @@
+# encoding: utf-8
 require "ca/version"
-
 module Ca
-  # Your code goes here...
+  class TextAnalitics
+
+    # Return result of +text+ analysis, +max_length+ is max lenght of phrase
+    #   TextAnalitics.analize("Alex have got cat") #=> {"Aleks" => 1, "have" => 1, "got" => 1, "cat" => 1}
+    #   TextAnalitics.analize("Blue onion", 2) #=> {"Blue" => 1, "onion" => 1, "Blue onion" => 1}
+    def self.analize(text, max_length = 1)
+      result = {}
+      words = text.split(separators)
+      (0...max_length).each do |words_per_phrase|
+        (words.size - words_per_phrase).times do |index|
+          range = (index..index + words_per_phrase)
+          phrase = words[range].join(" ")
+          result[phrase] = 0 unless result.key?(phrase)
+          result[phrase] += 1
+        end
+      end
+      result
+    end
+
+  private
+    # Return Regural Expresion for white symbols
+    #   separators #=> %r{[\s,.]+}
+    def self.separators
+      %r{[\s,.]+}
+    end
+
+
+  end
 end
---
+++ spec/ca/ca_spec.rb
@@ -0,0 +1,91 @@
+# encoding: utf-8
+require "ca"
+TEXT = "Lorem Lorem ipsum dolor sit amet, na na na na na na Foo Bar consectetur adipiscing-elit. Quisque Foo Bar ac nisi nunc, sit amet commodo mi. Nunc velit tellus, malesuada vitae vehicula a, imperdiet sed nisi."
+PL_TEXT = "Litwo! Ojczyzno moja! Ty jesteś jak zdrowie. Nazywał się kojarz wspaniały domów sojusz - mawiał - rzekł na kształt ogrodowych grządek:"
+HTML_TEXT = "<p>Lorem ipsum dolor sit amet tellus. Vestibulum ante felis, feugiat quam placerat id, nunc. Ut pharetra elementum. Fusce eu eros. Maecenas tortor venenatis in, convallis diam at arcu ac eros ac eros. Ut sodales, dictum vel, pellentesque eget, molestie justo euismod mi. Suspendisse rutrum magna suscipit enim. Duis a luctus diam. Donec elementum orci luctus quam at augue. Maecenas eget orci sit amet, tempor diam. Proin scelerisque rhoncus a, pellentesque auctor congue quis, tincidunt at, mauris. Pellentesque egestas dignissim, sapien sed nunc ut tellus porttitor ullamcorper, enim sed tortor. Ut tempus ipsum. Vestibulum ante ipsum primis in orci luctus et magnis dis parturient montes, nascetur ridiculus mus. Fusce condimentum magna dictum eu, commodo ipsum ac nibh massa quis leo. Ut aliquet, lacus lacus sed viverra accumsan. Fusce urna luctus tellus tortor, fermentum in, elementum consequat. Aliquam nonummy, mi. Cras sit amet dui at justo. Vivamus hendrerit tellus consectetuer elit. Nunc elementum. Fusce interdum. Donec suscipit lectus. Vivamus a metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Sed metus hendrerit laoreet. Nam ac turpis mauris.</p>"
+
+
+describe :Ca do
+  describe :TextAnalitics do
+
+    context "analize exemplary text with one word for English text" do
+
+      before(:all) do
+        @result = Ca::TextAnalitics.analize(TEXT)
+      end
+
+      it "method analize should return Hash" do
+        @result.should be_a(Hash)
+      end
+
+      it "method analize should return Hash with length > 0" do
+        @result.length.should be > 0
+      end
+
+      it "method analize shoudl return value 2 for 'Lorem' hash key" do
+        @result["Lorem"].should be(2)
+      end
+
+    end
+
+
+    context "analize exemplary text with dwo words in phrase for English text" do
+
+      before(:all) do
+        @result = Ca::TextAnalitics.analize(TEXT, 2)
+      end
+
+      it "method analize should return Hash" do
+        @result.should be_a(Hash)
+      end
+
+      it "method analize should return value 2 for 'Foo Bar' hash key" do
+        @result["Foo Bar"].should be(2)
+      end
+
+      it "method analize should return value 1 for 'a' hash key" do
+        @result["a"].should be(1)
+      end
+    end
+
+    context "analize exemplary text with tree words in phrase for English text" do
+
+      before(:all) do
+        @result = Ca::TextAnalitics.analize(TEXT, 3)
+      end
+
+      it "method analize should return Hash" do
+        @result.should be_a(Hash)
+      end
+
+      it "method analize should return 2 for 'na na na'" do
+        @result["na na na"].should be(4)
+      end
+
+      it "method analize should return 3 for 'na na'" do
+        @result["na na"].should be(5)
+      end
+    end
+
+    context "analize exemplary text with one word in phrase for Polish text" do
+
+      before(:all) do
+        @result = Ca::TextAnalitics.analize(PL_TEXT)
+      end
+
+      it "method analize should return Hash" do
+        @result.should be_a(Hash)
+      end
+
+      it "method analize should return 1 for 'domów'" do
+        @result["domów"].should be(1)
+      end
+
+      it "method analize should return Hash with length > 0" do
+        @result.length.should be > 0
+      end
+
+    end
+  end
+
+end
