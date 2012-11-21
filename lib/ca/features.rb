e1397c2c7e6c75060a9660dfb86e097d7a22c744
Wojciech Bożek <bob@bob-G31M-ES2L.(none)>
2012-11-21 18:10:00 +0100

    Uncomited changes previosus

--- lib/ca/features.rb
+++ lib/ca/features.rb
@@ -33,16 +33,16 @@ module Ca
     # Construct +weight+ is tag Array for example [:li, :strong, :u]
     # +position+ of phrase
     # +length+ is a phrase words number
-    def initialize(weight, position, length)
-      @frequency, @words_count = 0, length
+    def initialize
+      @frequency = 0
       @positions, @weights, @warning = [], [], []
-      update(weight, position)
     end
 
     # Method that update informations of phrase
     # +weight+ is a tag Array, same as initialize
     # +position+ of phrase
-    def update(weight, position)
+    def update(weight, position, length)
+      @words_count = length
       if @positions.include? position
         index = @positions.index position
         @weights.at(index) << weight unless @weights.at(index).include?(weight)
